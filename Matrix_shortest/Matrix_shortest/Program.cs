145d686e0a3e5419556ad58db42b9691b20a0534
LyndinK <kirill.lyndin@gmail.com>
2018-07-02 20:23:41 +0300

    Initial commit

---
+++ Matrix_shortest/Matrix_shortest/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Matrix_shortest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] field = { {".", ".", "#", ".", ".", ".", ".", ".", ".", ".", },
+                                {".", ".", "#", ".", ".", ".", ".", ".", ".", ".", },
+                                {".", ".", "#", ".", "#", ".", ".", ".", ".", ".", },
+                                {".", ".", "#", ".", "#", "X", ".", ".", ".", ".", },
+                                {".", ".", "#", ".", "#", ".", ".", ".", ".", ".", },
+                                {".", ".", "#", ".", "#", "#", "#", "#", "#", "#", },
+                                {".", ".", "#", ".", ".", ".", ".", ".", ".", ".", },
+                                {".", ".", "#", "#", "#", ".", "#", "#", "#", ".", },
+                                {".", ".", ".", "0", ".", ".", ".", ".", ".", ".", },
+                                {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", },};
+            int[,] paths = new int[10, 10];
+
+            List<int> findvalue(string to_find)
+            {
+                List<int> res = new List<int>();
+                for (int a=0; a < field.GetLength(0); a++)
+                {
+                    for (int j = 0; j < field.GetLength(1); j++)
+                    {
+                        if (String.Equals(field[a, j], to_find))
+                        {
+                            res.Add(a);
+                            res.Add(j);
+                        }
+                    }
+                }
+                return res;
+            }
+            var start = findvalue("0");
+            paths[start[0], start[1]] = 1;
+            int value = 1;
+            var end = findvalue("X");
+            void try_to_fill (int x, int y,int dx, int dy, int val)
+            {
+                if (0<=x+dx && 9>=x+dx && 0<=y+dy && 9>=y+dy && paths[x + dx, y + dy]== 0 && field[x + dx, y + dy]!="#")
+                {
+                    paths[x + dx, y + dy] = val;
+                }
+            }
+            void try_to_move(int dx, int dy)
+            {
+                if (0 <= end[0] + dx && 9 >= end[0] + dx && 0 <= end[1] + dy && 9 >= end[1] + dy && paths[end[0] + dx, end[1] + dy] == value-1)
+                {
+                    end[0] += dx;
+                    end[1] += dy;
+                    field[end[0], end[1]] = "*";
+                    value = value - 1;
+                }
+            }
+            var now = DateTime.Now.AddSeconds(15);
+            while (paths[end[0], end[1]] == 0)
+            {
+                if (DateTime.Now > now)
+                {
+                    Console.WriteLine("the path doesn't seem to exist");
+                    break;
+                }
+                for (int a = 0; a < paths.GetLength(0); a++)
+                {
+                    for (int j = 0; j < paths.GetLength(1); j++)
+                    {
+                        if (paths[a, j] == value)
+                        {
+                            try_to_fill(a, j, 0, 1, value + 1);
+                            try_to_fill(a, j, 1, 0, value + 1);
+                            try_to_fill(a, j, -1, 0, value + 1);
+                            try_to_fill(a, j, 0, -1, value + 1);
+                        }
+                    }
+                }
+                value++;
+            }
+            value = paths[end[0], end[1]];
+            while (value > 2)
+            {
+                try_to_move(0, 1);
+                try_to_move(1, 0);
+                try_to_move(-1, 0);
+                try_to_move(0, -1);
+            }
+
+            for (int a = 0; a < paths.GetLength(0); a++)
+            {
+                Console.Write("\n");
+                for (int j = 0; j < paths.GetLength(1); j++)
+                {
+                    Console.Write(paths[a, j] + " ");
+                }
+            }
+            Console.Write("\n");
+            Console.Write("\n");
+
+            for (int a = 0; a < field.GetLength(0); a++)
+            {
+                Console.Write("\n");
+                for (int j = 0; j < field.GetLength(1); j++)
+                {
+                    Console.Write(field[a, j] + " ");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Matrix_shortest/Matrix_shortest/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Matrix_shortest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Matrix_shortest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("274ad0fa-1483-42aa-b29c-c62a68408aec")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
